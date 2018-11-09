5ff9dd11bd0f2524ded2b9d2618ebad5c68959df
Sean Langton <slangton1@gmail.com>
2018-11-09 19:44:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,7 @@ class Song
 
 
   def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name).add_song(self)
+    self.artist = Artist.find_or_create_by_name(name)
   end
 
   def self.new_by_filename(filename)
